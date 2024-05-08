a5d20508a4db448d8e5e02138c91e7c1fab6b282
Hameem-K <hkarimnyc08@gmail.com>
2024-05-08 10:43:36 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
 import pygame
 pygame.init()
-game_screen = pygame.display.set_mode((1000,1000))
+game_screen = pygame.display.set_mode((736,736))
+
+background = pygame.image.load('space.png')
+
+pygame.init()
+pygame.font.init()
+my_font = pygame.font.SysFont('Comic Sans', 12)
+pygame.display.set_caption("Space Invaders: Beginner Edition")
+
+run = True
+while run:
+    game_screen.blit(background,(0,0))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    pygame.display.update()
