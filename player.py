7587818723983ea0423045fb57e580baeb2a395c
Hameem-K <hkarimnyc08@gmail.com>
2024-05-13 10:47:22 -0400

    Update player.py
    Restarting player.py

--- player.py
+++ player.py
@@ -1,11 +1,1 @@
-import pygame
 
-class Player:
-
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-        self.image = pygame.image.load('player_sprite.png')
-        self.image_size = (50, 50)
-        self.rect = pygame.Rect(self.x, self.y, self.image_size[0], self.image_size[1])
-        
