d9b230a27c9d0e10f67eb1a679e101b080ce04f2
Hameem-K <hkarimnyc08@gmail.com>
2024-05-13 10:51:21 -0400

    Update player.py

--- player.py
+++ player.py
@@ -1,1 +1,18 @@
+import pygame
 
+class Player:
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.image = pygame.image.load('player_sprite.png')
+        self.image_size = self.image.get_size()
+        self.rect = pygame.Rect(self.x, self.y, self.image_size[0], self.image_size[1])
+        self.delta = 5
+        self.current_direction = "right"
+
+    def move_direction(self, direction):
+        if self.current_direction == "right":
+            self.x += self.delta
+        elif self.current_direction == "left":
+            self.x -= self.delta
