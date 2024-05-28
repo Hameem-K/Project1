714dfc39c1b3d4a44ddad9540793a0596c914bfb
Hameem-K <hkarimnyc08@gmail.com>
2024-05-28 10:59:09 -0400

    Update laser.py

--- laser.py
+++ laser.py
@@ -1,1 +1,13 @@
+import pygame
+
+
+class Laser:
+    def __init__(self, x, y, color, direction):
+        self.x = x
+        self.y = y
+        self.color = color
+        self.direction = color
+        self.image = pygame.Surface((5, 10))
+        self.image.fill(self.color)
+        self.rect = self.image.get_rect(topleft=(self.x, self.y))
 
