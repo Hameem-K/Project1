6094ffd6064c7633f4a1275edae236c15cb4bd18
Hameem-K <hkarimnyc08@gmail.com>
2024-05-29 11:00:00 -0400

    Update enemy.py

--- enemy.py
+++ enemy.py
@@ -1,16 +1,19 @@
 import pygame
+from laser import Laser
 
 
 class Enemy:
-
     def __init__(self, x, y):
         self.x = x
         self.y = y
         self.image = pygame.image.load('enemy_sprite.png')
         self.image_size = self.image.get_size()
         self.rect = pygame.Rect(self.x, self.y, self.image_size[0], self.image_size[1])
-        self.delta = 3.75
+        self.delta = 4.5
         self.direction = "right"
+        self.lasers = []
+        self.last_shot_time = 0
+        self.shot_delay = 1500
 
     def move(self):
         if self.direction == "right":
@@ -22,3 +25,20 @@ class Enemy:
             if self.x <= 0:
                 self.direction = "right"
         self.rect = pygame.Rect(self.x, self.y, self.image_size[0], self.image_size[1])
+
+    def shooting_mech(self):
+        current_time = pygame.time.get_ticks()
+        if current_time - self.last_shot_time > self.shot_delay:
+            laser = Laser(self.x + self.image_size[0] // 2, self.y + self.image_size[1], (255, 0, 0), "down")
+            self.lasers.append(laser)
+            self.last_shot_time = current_time
+
+    def draw_lasers(self, game_screen):
+        for laser in self.lasers:
+            laser.draw(game_screen)
+
+    def move_lasers(self):
+        for laser in self.lasers:
+            laser.move()
+            if laser.y > 1000:
+                self.lasers.remove(laser)
