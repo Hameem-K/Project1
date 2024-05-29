ae16c382e099fa82b6ebf9fd6451c2798cc4f454
Hameem-K <hkarimnyc08@gmail.com>
2024-05-29 10:59:01 -0400

    Update main.py

--- main.py
+++ main.py
@@ -6,14 +6,13 @@ from laser import Laser
 pygame.init()
 game_screen = pygame.display.set_mode((1000, 1000))
 pygame.font.init()
-my_font = pygame.font.SysFont('Comic Sans MS', 20)
+my_font = pygame.font.SysFont('Comic Sans MS', 19)
 pygame.display.set_caption("Space Wars")
 background = pygame.image.load('background.png')
 player = Player(450, 900)
 enemy = Enemy(500, 0)
 enemy2 = Enemy(200, 0)
-enemy3 = Enemy(350, 0)
-enemy4 = Enemy(650, 0)
+
 run = True
 
 
@@ -23,14 +22,15 @@ def intro_screen():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
-                quit()
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
                     intro = False
         game_screen.blit(background, (0, 0))
-        intro_msg = my_font.render("Press the spacebar to start, A to go left, and D to go right (or use the arrow keys)", True,
-                                   (255, 255, 255))
-        game_screen.blit(intro_msg, (126, 500))
+        intro_msg = my_font.render(
+            "Press the spacebar to start, A to go left, D to go right (or use the arrow keys) and up arrow key to shoot",
+            True,
+            (255, 255, 255))
+        game_screen.blit(intro_msg, (35, 0))
         pygame.display.update()
 
 
@@ -50,13 +50,9 @@ while run:
 
     enemy.move()
     enemy2.move()
-    enemy3.move()
-    enemy4.move()
 
     game_screen.blit(player.image, player.rect)
     game_screen.blit(enemy.image, enemy.rect)
     game_screen.blit(enemy2.image, enemy2.rect)
-    game_screen.blit(enemy3.image, enemy3.rect)
-    game_screen.blit(enemy4.image, enemy4.rect)
 
     pygame.display.update()
